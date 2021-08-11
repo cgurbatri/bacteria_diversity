4a135db0d43d29b0a3f512da9826cb6524a13dde
cgurbatri <crg2160@columbia.edu>
2021-08-11 19:27:56 -0400

    dev 1

--- data-12-4-3-resources/plots.js
+++ data-12-4-3-resources/plots.js
@@ -29,7 +29,6 @@ function init() {
       var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
       var result= resultsarray[0]
       var PANEL = d3.select("#sample-metadata");
-      
       PANEL.html("");
       Object.entries(result).forEach(([key, value]) => {
         PANEL.append("h6").text(`${key}: ${value}`);
