f775b89271d4f2618b11cf138144df54ba021435
cgurbatri <crg2160@columbia.edu>
2021-08-14 13:09:56 -0400

    gauge

--- chart.js
+++ chart.js
@@ -27,8 +27,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
-  
-}
+  }
 
 // Demographics Panel 
 function buildMetadata(sample) {
@@ -58,16 +57,16 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-      var sampleData = data.samples;
+    var sampleData = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-      var filteredArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+    var filteredArray = sampleData.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var result = filteredArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-      var otu_ids = result.otu_ids;
-      var otu_labels = result.otu_labels;
-      var sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -114,6 +113,47 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
-    
-  });
+
+// Gauge Chart https://plotly.com/javascript/gauge-charts/
+function gauge(dataID) {
+  var data = [
+      {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: wfreq, //Washing frequency
+          title: { text: "Washing frequency" },
+          type: "indicator",
+
+          mode: "gauge+number+delta",
+          delta: { reference: 4, increasing: { color: 'green' } },
+          gauge: {
+              axis: { range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },
+              bar:{color: 'blue'},
+              steps: [
+                  { range: [0, 4], color: "red" },
+                  { range: [4, 9], color: "green" }
+              ],
+              threshold: {
+                  line: { color: "grey", width: 4 },
+                  thickness: 1,
+                  value: 9
+              }
+          },
+          bgcolor: "lavender",
+      }
+  ];
+  var layout = {
+      width: 200,
+      height: 370,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      paper_bgcolor: "lavender",
+      font: { color: "darkblue", family: "Arial" }
+  };
+
+  
+  Plotly.newPlot('gauge', data, layout);
 }
+horizontalChart(sampleId)
+bubbleChart(sampleId)
+gauge(sampleId)
+})
+};
