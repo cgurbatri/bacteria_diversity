2c680744337590d07f87242062e713f4e10da6f6
cgurbatri <crg2160@columbia.edu>
2021-08-14 15:09:10 -0400

    gauge

--- chart.js
+++ chart.js
@@ -115,16 +115,16 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
 // Gauge Chart https://plotly.com/javascript/gauge-charts/
-    var wfreq = parseInt(resultArray)
+
+    var wfreq = parseInt(filteredArray)
     var gaugeData = [
         {
             domain: { x: [0, 1], y: [0, 1] },
             value: wfreq, //Washing frequency
             title: { text: "Washing frequency" },
             type: "indicator",
-
             mode: "gauge+number",
-            delta: { reference: 4, increasing: { color: 'green' } },
+            // delta: { reference: 4, increasing: { color: 'green' } },
             gauge: {
                 axis: { range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },
                 bar:{color: 'blue'},
@@ -144,11 +144,12 @@ function buildCharts(sample) {
     var gaugeLayout = {
         width: 200,
         height: 370,
-        margin: { t: 25, r: 25, l: 25, b: 25 },
-        font: { color: "darkblue", family: "Arial" }
+        // margin: { t: 25, r: 25, l: 25, b: 25 },
+        // font: { color: "darkblue", family: "Arial" }
     };
 
 
     Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+    
 })
 };
